f6902bada496d33dfe7fbf6fe3d7983e07e2021c
Michelle Frankot <michelle.frankot@gmail.com>
2026-03-28 15:32:22 -0700

    customized axis labels

--- Plot_AEs.R
+++ Plot_AEs.R
@@ -15,21 +15,42 @@ library(ggplot2) # for plotting
 
 
 
+
+
+### Constants and Labels ###
+
+AEs <- c("Symptomatic Hypotension", 
+            "Symptomatic Hypotension (Systolic <90 mmHG)",
+            "Serum Creatinine >=2.5 mg/dl", 
+            "Serum Creatinine >=3.0 mg/dl",  
+            "Serum Potassium >5.5 mmol/L",
+            "Serum Potassium >6.0 mmol/L",
+            "Cough",
+            "Angioedema (No Tx or AH Only)",
+            "Angioedema (CA or GCs)",
+            "Angioedema (Hospitalized; No Airway Compromise)",
+            "Angioedema (Hospitalized; Airway Compromise)")
+
+
+AE_Labels <- c("Symptomatic Hypotension", 
+            "Systolic Blood Pressure < 90 mmHG",
+            expression("Serum Creatinine ">="2.5 mg/dl"), 
+            expression("Serum Creatinine ">="3.0 mg/dl"),   
+            "Serum Potassium > 5.5 mmol/L",
+            "Serum Potassium > 6.0 mmol/L",
+            "Cough",
+            "Angioedema: Antihistamines or No Tx",
+            "Angioedema: Catecholamines or GCs",
+            "Angioedema: Hospitalized")
+
+
+
+
+
 ### Create dataframe from Table 3 ###
 
 data <- data.frame(
-  Event = rep(c("Symptomatic Hypotension", 
-                "Symptomatic Hypotension (Systolic <90 mmHG)",
-                "Serum Creatinine >=2.5 mg/dl", 
-                "Serum Creatinine >=3.0 mg/dl",  
-                "Serum Potassium >5.5 mmol/L",
-                "Serum Potassium >6.0 mmol/L",
-                "Cough",
-                "Angioedema (No Tx or AH Only",
-                "Angioedema (CA orGCs; No Hospitalization",
-                "Angioedema (Hospitalization; No Airway Compromise",
-                "Angioedema (Airway Compromise)"), 
-              times = 1, each = 2),
+  Event = rep(AEs, times = 1, each = 2),
   Group = rep(c("LCZ696", "Enalapril"), times = 11),
   n = c(588, 388, 112, 59, 139, 188, 63, 83, 674, 727, 
         181, 236, 474, 601, 10, 5, 6, 4, 3, 1, 0, 0),
@@ -43,6 +64,8 @@ data <- data.frame(
 to.plot <- data %>%
   mutate(Total_N = ifelse(Group == "LCZ696", 4187, 4212))%>%
   mutate(Percent = round(100*(n/Total_N), digits = 1))%>%
+  mutate(Group = factor(Group, levels = c("LCZ696", "Enalapril")),
+    Event = factor(Event, levels = rev(AEs)))%>%
   filter(Event != "Angioedema (Airway Compromise)")
 
 
@@ -58,14 +81,17 @@ ggplot(data = to.plot,
            position = "dodge",
            )+
   coord_flip()+
-  geom_text(aes(label = paste0("n=", n)),
-            position = position_dodge(width = 1),
-            hjust = -0.3, 
-            size = 4)+
+  # geom_text(aes(label = paste0("n=", n)),
+  #           position = position_dodge(width = 1),
+  #           hjust = -0.3, 
+  #           size = 4)+ # option to add sample size labels
   ggtitle("Adverse Events During Treatment")+
   ylab("Experienced Event (%)")+
   xlab("")+
   scale_fill_manual(values = c("#590E36", "#E0C1D2"))+
+#  scale_x_discrete(labels = scales::label_wrap(28)) +
+  scale_x_discrete(labels = rev(AE_Labels))+
+  ylim(c(0, 20))+
   theme_classic(base_size = 16)+
   theme(legend.position = "bottom")
 
